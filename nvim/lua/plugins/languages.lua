4634e6e5a69b71d375350c1a7f5344ed89b66dfb
Trần Thiên Minh <118097966+imchocomint@users.noreply.github.com>
2025-06-11 10:25:22 +0700

    Update languages.lua

--- nvim/lua/plugins/languages.lua
+++ nvim/lua/plugins/languages.lua
@@ -2,19 +2,6 @@
 return {
 
   {
-    'ray-x/go.nvim',
-    dependencies = {
-      'ray-x/guihua.lua',
-      'neovim/nvim-lspconfig',
-      'nvim-treesitter/nvim-treesitter',
-    },
-    config = function()
-      require('go').setup()
-    end,
-  },
-  
-
-  {
     "OXY2DEV/markview.nvim",
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
